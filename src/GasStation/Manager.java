103fae1010537d33461cf322094b0e8afd44e1b1
Eli Musgrove <eli.musgrove9@gmail.com>
2020-10-13 17:14:58 -0500

    Update.

--- src/GasStation/Manager.java
+++ src/GasStation/Manager.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Manager {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    public static void main(String[] args) throws SQLException {
         System.out.println("manage");
 
         // Example database connection
