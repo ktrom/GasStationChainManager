4800ddc2764775530042178e1d4b40bcbb55db89
Eli Musgrove <eli.musgrove9@gmail.com>
2020-10-14 02:05:06 -0500

    Finish database create script.

--- src/DatabaseClasses/DatabaseSetup.java
+++ src/DatabaseClasses/DatabaseSetup.java
@@ -5,95 +5,97 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DatabaseSetup {
+
+    /**
+     * Database connection instance.
+     */
     private static Connection conn;
 
     public DatabaseSetup(Connection conn) {
         this.conn = conn;
     }
 
+    /**
+     * Setup all initial database tables if not already in place.
+     *
+     * @throws SQLException
+     */
     public void initializeDatabase() throws SQLException {
         createGasStationTable();
-        createInventoryTable();
-        createItemTable();
-        createItemDescriptionTable();
         createEmployeeTable();
+        createItemTable();
+        createInventoryTable();
         createScheduleTable();
     }
 
     private void createGasStationTable() throws SQLException {
         Statement stmt = conn.createStatement();
 
-        String sql = "CREATE TABLE IF NOT EXISTS GasStation " +
-                "(GasStationID INT PRIMARY KEY AUTO_INCREMENT, " +
-                " Location VARCHAR(20)) ";
+        String sql =
+                "CREATE TABLE IF NOT EXISTS GasStation(" +
+                    "GasStationID INT UNSIGNED PRIMARY KEY AUTO_INCREMENT," +
+                    "Location VARCHAR(50)\n" +
+                ")";
         stmt.executeUpdate(sql);
-
     }
 
-    private void createInventoryTable() throws SQLException{
+    private void createEmployeeTable() throws SQLException {
         Statement stmt = conn.createStatement();
 
-        String sql = "CREATE TABLE IF NOT EXISTS Inventory " +
-                "(InventoryID INT PRIMARY KEY AUTO_INCREMENT, " +
-                "GasStationID INT, " +
-                "FOREIGN KEY (GasStationId) REFERENCES GasStation(GasStationID))";
-
+        String sql =
+                "CREATE TABLE IF NOT EXISTS Employee(" +
+                    "EmployeeID INT UNSIGNED PRIMARY KEY AUTO_INCREMENT," +
+                    "GasStationID INT UNSIGNED NOT NULL," +
+                    "Name VARCHAR(100) NOT NULL," +
+                    "SSN VARCHAR(11) NOT NULL," +
+                    "Salary DOUBLE NOT NULL," +
+                    "Department VARCHAR(20)," +
+                    "EmployeePosition ENUM('coo', 'cfo', 'manager', 'hiring_manager', 'attendant') NOT NULL," +
+                    "StartDate TIMESTAMP NOT NULL DEFAULT NOW()," +
+                    "FOREIGN KEY(GasStationID) REFERENCES GasStation(GasStationID)" +
+                ")";
         stmt.executeUpdate(sql);
     }
 
     private void createItemTable() throws SQLException {
         Statement stmt = conn.createStatement();
 
-        String sql = "CREATE TABLE IF NOT EXISTS Item " +
-                "(ItemID INT PRIMARY KEY AUTO_INCREMENT, " +
-                "InventoryID INT, " +
-                "FOREIGN KEY (InventoryId) REFERENCES Inventory(InventoryID))";
-
+        String sql =
+                "CREATE TABLE IF NOT EXISTS Item(" +
+                    "ItemID INT UNSIGNED PRIMARY KEY AUTO_INCREMENT," +
+                    "Name VARCHAR(20) NOT NULL," +
+                    "Price DOUBLE NOT NULL," +
+                    "PhotoURL TEXT," +
+                    "Notes TEXT" +
+                ")";
         stmt.executeUpdate(sql);
     }
 
-    private void createItemDescriptionTable() throws SQLException{
+    private void createInventoryTable() throws SQLException {
         Statement stmt = conn.createStatement();
 
-        String sql = "CREATE TABLE IF NOT EXISTS ItemDescription " +
-                "(ItemDescriptionID INT PRIMARY KEY AUTO_INCREMENT, " +
-                "ItemID INT, " +
-                "FOREIGN KEY (ItemId) REFERENCES Item(ItemID), " +
-                "ItemName VARCHAR(20), " +
-                "Price DOUBLE, " +
-                "Quantity DOUBLE, " +
-                "PhotoURL VARCHAR(50), " +
-                "Notes VARCHAR(50)) ";
-
+        String sql =
+                "CREATE TABLE IF NOT EXISTS Inventory(" +
+                    "GasStationID INT UNSIGNED," +
+                    "ItemID INT UNSIGNED," +
+                    "Quantity INT UNSIGNED NOT NULL DEFAULT 0," +
+                    "FOREIGN KEY(ItemID) REFERENCES Item(ItemID)," +
+                    "FOREIGN KEY(GasStationID) REFERENCES GasStation(GasStationID)," +
+                    "PRIMARY KEY(GasStationID, ItemID)" +
+                ")";
         stmt.executeUpdate(sql);
     }
 
-    private void createEmployeeTable() throws SQLException{
+    private void createScheduleTable() throws SQLException {
         Statement stmt = conn.createStatement();
 
-        String sql = "CREATE TABLE IF NOT EXISTS Employee " +
-                "(EmployeeID INT PRIMARY KEY AUTO_INCREMENT, " +
-                "EmployeeName VARCHAR(20), " +
-                "Salary DOUBLE, " +
-                "Location VARCHAR(20), " +
-                "Department VARCHAR (20), " +
-                "EmployeePosition VARCHAR(20), " +
-                "PhotoURL VARCHAR(20))";
-
-        stmt.executeUpdate(sql);
-    }
-
-    private void createScheduleTable() throws SQLException{
-        Statement stmt = conn.createStatement();
-
-        String sql = "CREATE TABLE IF NOT EXISTS Schedule " +
-                "(GasStationID INT, " +
-                "FOREIGN KEY (GasStationID) REFERENCES GasStation(GasStationID), " +
-                "EmployeeID INT, " +
-                "FOREIGN KEY (EmployeeID) REFERENCES Employee(EmployeeID), " +
-                "CalenderDate INT, " +
-                "Shift INT)";
-
+        String sql =
+                "CREATE TABLE IF NOT EXISTS Schedule(" +
+                    "GasStationID INT UNSIGNED," +
+                    "EmployeeID INT UNSIGNED," +
+                    "Date DATE NOT NULL," +
+                    "Shift ENUM('1', '2', '3') NOT NULL" +
+                ")";
         stmt.executeUpdate(sql);
     }
 }
