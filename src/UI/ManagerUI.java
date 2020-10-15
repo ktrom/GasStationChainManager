59c3c461e699a048e466f315141c17c9bf6650ed
trommmah <trom.kyle@gmail.com>
2020-10-15 14:30:03 -0500

    added conditions

--- src/UI/ManagerUI.java
+++ src/UI/ManagerUI.java
@@ -60,39 +60,43 @@ public class ManagerUI {
         GasStationController gsc = new GasStationController();
         ArrayList<Employee> availableEmployees = gsc.getEmployees(gasStationID);
 
-        System.out.println("Which Employee would you like to schedule?");
-        Iterator<Employee> i = availableEmployees.iterator();
-        int j = 1;
-        while (i.hasNext()) {
-            Employee nextEmployee = i.next();
-            System.out.println(j + ": " + nextEmployee.getName());
-            j++;
+        if (availableEmployees.size() == 0) {
+            System.out.print("No employees in system!");
+        } else {
+            System.out.println("Which Employee would you like to schedule?");
+            Iterator<Employee> i = availableEmployees.iterator();
+            int j = 1;
+            while (i.hasNext()) {
+                Employee nextEmployee = i.next();
+                System.out.println(j + ": " + nextEmployee.getName());
+                j++;
+            }
+            int selectedIndex = s.nextInt() - 1;
+
+            Employee selectedEmployee = availableEmployees.get(selectedIndex);
+
+            int employeeId = selectedEmployee.getEmployeeID();
+
+            String date = "";
+
+            System.out.println("What day would you like to schedule the employee for? (YYYY-MM-DD)");
+            date = s.next();
+
+            Date d = Date.valueOf(date);
+
+            System.out.println("What shift would you like to schedule the employee for?");
+            System.out.println("1: 7-3");
+            System.out.println("2: 3-11");
+            System.out.println("3: 11-7");
+
+            System.out.println("Enter 1, 2, or 3: ");
+            int shift = s.nextInt();
+
+            ScheduleController sc = new ScheduleController();
+            sc.scheduleEmployee(gasStationID, employeeId, d, shift);
+
+            printSchedule(managerID);
         }
-        int selectedIndex = s.nextInt() - 1;
-
-        Employee selectedEmployee = availableEmployees.get(selectedIndex);
-
-        int employeeId = selectedEmployee.getEmployeeID();
-
-        String date = "";
-
-        System.out.println("What day would you like to schedule the employee for? (YYYY-MM-DD)");
-        date = s.next();
-
-        Date d = Date.valueOf(date);
-
-        System.out.println("What shift would you like to schedule the employee for?");
-        System.out.println("1: 7-3");
-        System.out.println("2: 3-11");
-        System.out.println("3: 11-7");
-
-        System.out.println("Enter 1, 2, or 3: ");
-        int shift = s.nextInt();
-
-        ScheduleController sc = new ScheduleController();
-        sc.scheduleEmployee(gasStationID, employeeId, d, shift);
-
-        printSchedule(managerID);
     }
 
     /**
@@ -111,27 +115,32 @@ public class ManagerUI {
         GasStationController gsc = new GasStationController();
         ArrayList<Employee> availableEmployees = gsc.getEmployees(gasStationID);
 
-        System.out.println("Which Employee would you like to assign a task to?");
-        Iterator<Employee> i = availableEmployees.iterator();
-        int j = 1;
-        while (i.hasNext()) {
-            Employee nextEmployee = i.next();
-            System.out.println(j + ": " + nextEmployee.getName());
-            j++;
+        if (availableEmployees.size() == 0){
+            System.out.print("No employees in system!");
         }
-        int selectedIndex = scan.nextInt() - 1;
+        else {
+            System.out.println("Which Employee would you like to assign a task to?");
+            Iterator<Employee> i = availableEmployees.iterator();
+            int j = 1;
+            while (i.hasNext()) {
+                Employee nextEmployee = i.next();
+                System.out.println(j + ": " + nextEmployee.getName());
+                j++;
+            }
+            int selectedIndex = scan.nextInt() - 1;
 
-        Employee selectedEmployee = availableEmployees.get(selectedIndex);
+            Employee selectedEmployee = availableEmployees.get(selectedIndex);
 
-        int employeeId = selectedEmployee.getEmployeeID();
+            int employeeId = selectedEmployee.getEmployeeID();
 
-        scan.nextLine();
-        String taskDescription;
-        System.out.println("Give a brief description of the task: ");
-        taskDescription = scan.nextLine();
+            scan.nextLine();
+            String taskDescription;
+            System.out.println("Give a brief description of the task: ");
+            taskDescription = scan.nextLine();
 
-        TaskController tc = new TaskController();
-        tc.createTask(gasStationID, employeeId, taskDescription);
+            TaskController tc = new TaskController();
+            tc.createTask(gasStationID, employeeId, taskDescription);
+        }
     }
 
     /**
